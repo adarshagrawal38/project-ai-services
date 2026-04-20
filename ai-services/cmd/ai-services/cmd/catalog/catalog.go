35015776a93fc1a8d0889fbd445d59acc4f224b2
Mayuka Channankaiah <Mayuka.C@ibm.com>
2026-04-20 20:28:19 +0530

    Catalog Configure command Implementation - 1 (#637)
    - Asset updates.
    - Introduce new Catalog configure sub command.
    - Read the admin Password as Prompt.
    
    This is actually broken down/ subset of this PR:
    https://github.com/IBM/project-ai-services/pull/627 as suggested by
    @yussufsh to have smaller PRs for better reviewing.
    
    Will be raising subsequent changes in multiple smaller parts.
    
    Signed-off-by: mayuka-c <Mayuka.C@ibm.com>

--- ai-services/cmd/ai-services/cmd/catalog/catalog.go
+++ ai-services/cmd/ai-services/cmd/catalog/catalog.go
@@ -15,6 +15,7 @@ deploying services, and managing service metadata`,
 	}
 
 	catalogCMD.AddCommand(NewAPIServerCmd())
+	catalogCMD.AddCommand(NewConfigureCmd())
 	catalogCMD.AddCommand(NewHashpwCmd())
 	catalogCMD.AddCommand(NewLoginCmd())
 	catalogCMD.AddCommand(NewLogoutCmd())
